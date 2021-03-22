fdc6c96bdf273615d428e383c5eb1d97f22e40ef
DevangSharma <debusharma1999@gmail.com>
2021-03-22 16:18:15 +0530

    upgrade to 2

--- android/app/src/main/kotlin/com/example/somewhere/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.SomeWhere
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-var colorPrimary = Colors.white;
-final voilet = Colors.white;
-final grey = Colors.grey;
-final yellow = Color(0xFF8A2BE2);
